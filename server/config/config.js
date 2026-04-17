62f17cd0eca7498ea70439e78a1fad88998a383c
Muhammad Moeen <mooenmalik374@gmail.com>
2026-04-17 11:18:21 +0500

    feat: added axios and react-hot-toast for API requests and notifications; update Navbar and AuthModal components

--- server/config/config.js
+++ server/config/config.js
@@ -12,6 +12,7 @@ if (!process.env.JWT_SECRET) {
 const configs = {
   MONGO_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  CLIENT_URL: process.env.CLIENT_URL,
   PORT: process.env.PORT,
 };
 
