ba8465edc63a1e58337df6fc7a3e2bbfdf4679d1
Muhammad Moeen <mooenmalik374@gmail.com>
2026-04-17 10:17:20 +0500

    feat: added todoData field to user model and include it in user creation response

---
+++ server/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    todoData: {
+      type: Array,
+      default: [],
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
